85b30cebfb044d62baa3e1ca47f8d11576c46b96
AMonem99 <103360597+AMonem99@users.noreply.github.com>
2022-10-07 18:07:04 +0500

    7/10/test

--- lib/basic_info/basic_info.dart
+++ lib/basic_info/basic_info.dart
@@ -4,7 +4,6 @@
 
 import 'dart:io';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
