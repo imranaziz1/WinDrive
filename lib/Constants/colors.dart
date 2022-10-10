4d27d5e7d1c303746bcd127a1da6736df0ca64f3
AMonem99 <103360597+AMonem99@users.noreply.github.com>
2022-10-10 13:12:32 +0500

    contants

---
+++ lib/Constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static const   kPrimaryColor = Colors.teal;
+  static const kBackGroundColor = Colors.white;
+  static const kSmallTextSize = 15;
+  static const kMediumTextSize = 20;
+  static const kLargeTextSize = 27;
+  static const kFontFamily = 'Poppins-LightItalic.ttf';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:win_drive/Constants/colors.dart';
 
 import 'Login/login_screen.dart';
+import 'WelcomeScreen/welcome_screen.dart';
 
 void main() { runApp(MyApp());}
 
@@ -9,7 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+theme: ThemeData(
+  primaryColor: Constants.kPrimaryColor,
 
+),
       home: MyHomePage(),
       debugShowCheckedModeBanner: false,
     );
